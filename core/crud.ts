f25a83650ac0982e12c729b4bc9d91316fe989ab
guilherme-se <guilhermeroroni21@gmail.com>
2023-12-05 21:45:17 -0300

    write files and clean db

--- core/crud.ts
+++ core/crud.ts
@@ -3,9 +3,44 @@ const DB_FILE_PATH = "core/db";
 
 console.log("crud");
 
+interface Todo{
+    date: string;
+    content: string;
+    done: boolean;
+}
+
 function create(content: string){
-    fs.writeFileSync(DB_FILE_PATH, content)
+    const todo: Todo  = {
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    };
+
+    console.log(todo);
+    
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ]
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos},null,2))
     return content;
 }
 
-console.log(create("testando msg nova"));
+function read(): Array<Todo>{
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if (!db.todos){ //teste de falha rapido
+        return[];
+    }
+    return db.todos
+}
+
+function CLEAR_DB(){
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+CLEAR_DB()
+create("segunda TODO")
+create("terceira TODO")
+console.log(read());
